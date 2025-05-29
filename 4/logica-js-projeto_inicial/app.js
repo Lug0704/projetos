0421923fad64fc1a49baef2457e13836155cdd9a
Lug0704 <luizgscunha0704@gmail.com>
2025-05-29 16:52:37 -0300

    desafio alura 3
    nao esta na ordem alura

---
+++ 4/logica-js-projeto_inicial/app.js
@@ -0,0 +1,20 @@
+
+alert("calculadora de notas ")
+
+alert("loadings...")
+
+let valor1= prompt ("qual sua primeira nota?")
+let valor2= prompt ("qual sua segunda nota?")
+let valor3= prompt ("qual sua terceira nota?")
+let mediaFinal=(valor1 / 3+valor2 /3 +valor3 /3)
+//let mediaFinal2=(mediaFinal)
+console.log(`${mediaFinal}`)
+if (mediaFinal2 >=70) {alert ("passou")}
+
+else (alert("reprovou") )
+let numeroAleatorio = Math.random()
+let numeroAleatorio1 =  parseInt (Math.random() *10 +1)
+let numeroAleatorio2 = parseInt (Math.random ()*1000 +1)
+console.log(`${numeroAleatorio}`)
+console.log(`${numeroAleatorio1}`)
+console.log(`${numeroAleatorio2}`)
