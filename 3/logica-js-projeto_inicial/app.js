c701d024b4291923d23d0739522871e41f434a18
Lug0704 <luizgscunha0704@gmail.com>
2025-05-29 16:13:52 -0300

    desafio alura 2
    nao esta na ordem da alura

---
+++ 3/logica-js-projeto_inicial/app.js
@@ -0,0 +1,9 @@
+let idade = prompt ("qual sua idade?") 
+if (idade>=18) {alert("voce e um maior de idade")}
+else {alert (" voce nao e um maior de idade")}
+let valor = prompt("me fale um numero")
+if (valor>=0) {alert ("esse numero e positivo")}
+else {alert ("esse e um numero negativo")}
+let newValor = 0
+while (newValor!= 10) {(newValor ++) 
+    console.log (`ò new valor e ${newValor}`)}
